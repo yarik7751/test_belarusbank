1d41a017eaa9a4f0db1de8e9b831ec11cfea52c3
yarik7751 <yaroslav.levshunov2@gmail.com>
2019-02-19 08:05:48 +0300

    +bottom navigation view

--- app/src/main/java/by/yarik/test_belarusbank/activity/MainActivity.java
+++ app/src/main/java/by/yarik/test_belarusbank/activity/MainActivity.java
@@ -1,24 +1,72 @@
 package by.yarik.test_belarusbank.activity;
 
 import android.os.Bundle;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+
+import com.aurelhubert.ahbottomnavigation.AHBottomNavigation;
+import com.aurelhubert.ahbottomnavigation.AHBottomNavigationItem;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import by.yarik.test_belarusbank.core.BaseActivity;
 import by.yarik.test_belarusbank.R;
 import by.yarik.test_belarusbank.screens.news.NewsFragment;
 
-public class MainActivity extends BaseActivity {
+public class MainActivity extends BaseActivity implements AHBottomNavigation.OnTabSelectedListener {
+
+    private static final int FIRST_ITEM = 0;
+    protected AHBottomNavigation bottomNavigation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initBottomNavigation();
+        for(AHBottomNavigationItem item : getBottomItems()) {
+            bottomNavigation.addItem(item);
+        }
 
         if (savedInstanceState == null) {
             setNewsFragment();
         }
     }
 
+    private void initBottomNavigation() {
+        bottomNavigation = findViewById(R.id.bottom_navigation);
+        bottomNavigation.setUseElevation(true);
+        bottomNavigation.setDefaultBackgroundColor(ContextCompat.getColor(this, android.R.color.white));
+        bottomNavigation.setBehaviorTranslationEnabled(false);
+        bottomNavigation.setAccentColor(ContextCompat.getColor(this, android.R.color.holo_green_light));
+        bottomNavigation.setInactiveColor(ContextCompat.getColor(this, android.R.color.holo_green_light));
+        bottomNavigation.setTitleState(AHBottomNavigation.TitleState.ALWAYS_SHOW);
+        bottomNavigation.setColored(false);
+        bottomNavigation.setCurrentItem(FIRST_ITEM);
+        bottomNavigation.setOnTabSelectedListener(this);
+        bottomNavigation.setTitleTextSizeInSp(14, 12);
+    }
+
+    private List<AHBottomNavigationItem> getBottomItems() {
+        List<AHBottomNavigationItem> items = new ArrayList<>();
+        items.add(new AHBottomNavigationItem(R.string.news_title, R.drawable.ic_news, R.color.colorPrimary));
+        items.add(new AHBottomNavigationItem(R.string.credits_title, R.drawable.ic_credits, R.color.colorPrimary));
+        items.add(new AHBottomNavigationItem(R.string.atm_title, R.drawable.ic_atm, R.color.colorPrimary));
+        return items;
+    }
+
     public void setNewsFragment() {
         setMainFragment(NewsFragment.newInstance());
+    }
+
+    @Override
+    public boolean onTabSelected(int position, boolean wasSelected) {
+        return false;
+    }
+
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
+
     }
 }
